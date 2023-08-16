18c00f0eb872d67b2c5203cce0de2d318ba88baf
akuppusamy-wci <99685732+akuppusamy-wci@users.noreply.github.com>
2023-08-16 16:24:12 -0400

    EVSRESTAPI-376 (#16)
    * Moved extracting gz and zip files to stardog_load.sh
    
    * Fixed getting file name when extracting from gz
    
    * Fixed getting transform scripts to use common work directory
    Made calling transformations generic
    
    * Added back optimize
    Added pre_condition_check to umlssemnet.sh

--- bin/transforms/hgnc.sh
+++ bin/transforms/hgnc.sh
@@ -4,9 +4,9 @@ TERMINOLOGY_URL="${3:-http://ncicb.nci.nih.gov/genenames.org/HGNC.owl}"
 dir=$(pwd | perl -pe 's#/cygdrive/c#C:#;')
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 EVS_OPS_HOME=$DIR/../..
-WORK_DIRECTORY=$EVS_OPS_HOME/work
-INPUT_DIRECTORY=$WORK_DIRECTORY/${TERMINOLOGY}_input
-OUTPUT_DIRECTORY=$WORK_DIRECTORY/${TERMINOLOGY}_output
+WORK_DIRECTORY=$EVS_OPS_HOME/bin/work
+INPUT_DIRECTORY=$WORK_DIRECTORY/input
+OUTPUT_DIRECTORY=$WORK_DIRECTORY/output
 VENV_DIRECTORY=$WORK_DIRECTORY/venv
 VENV_BIN_DIRECTORY=$VENV_DIRECTORY/bin
 # We sort on version to determine "latest" programmatically in a consistent way
@@ -20,6 +20,17 @@ cleanup() {
     exit $code
   fi
 }
+pre_condition_check(){
+if [ ! -d $INPUT_DIRECTORY ]; then
+  echo "No input directory ($INPUT_DIRECTORY) found. Exiting"
+  cleanup 1
+fi
+
+if [ ! -d $OUTPUT_DIRECTORY ]; then
+  echo "No output directory ($OUTPUT_DIRECTORY) found. Exiting"
+  cleanup 1
+fi
+}
 setup() {
   python3 -m venv "$VENV_DIRECTORY"
   source "$VENV_BIN_DIRECTORY"/activate
@@ -27,8 +38,6 @@ setup() {
   pushd "$EVS_OPS_HOME" || exit
   "$VENV_BIN_DIRECTORY"/poetry install
   popd "$EVS_OPS_HOME" || exit
-
-  mkdir "$OUTPUT_DIRECTORY"
 }
 
 generate_standard_format_files() {
@@ -45,8 +54,8 @@ generate_owl_file() {
   echo "$versioned_owl_file"
 }
 
+pre_condition_check
 setup
 generate_standard_format_files "$@"
 versioned_owl_file=$(generate_owl_file)
-cleanup
 echo "$versioned_owl_file"
