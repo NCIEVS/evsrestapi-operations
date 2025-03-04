9e2aabf8ff2798c96a4e7e26c415f75fcea75828
akuppusamy-wci <akuppusamy@westcoastinformatics.com>
2025-03-03 16:58:10 -0800

    Removing password from echo

--- bin/stardog_load.sh
+++ bin/stardog_load.sh
@@ -415,7 +415,7 @@ load_data() {
   if [[ $l_graph_db_type == "stardog" ]]; then
     $l_graph_db_home/bin/stardog data add $db -g $graph $file -u $l_graph_db_username -p $l_graph_db_password | sed 's/^/    /'
   elif [[ $l_graph_db_type == "jena" ]]; then
-    echo "    curl -i -s -u ${l_graph_db_username}:$l_graph_db_password -f -X POST -H \"Content-Type: application/rdf+xml\" -T $file $l_graph_db_url/$db/data?graph=$graph"
+    echo "    curl -i -s -u ${l_graph_db_username}:**** -f -X POST -H \"Content-Type: application/rdf+xml\" -T $file $l_graph_db_url/$db/data?graph=$graph"
     curl -i -s -u "${l_graph_db_username}:$l_graph_db_password" -f -X POST -H "Content-Type: application/rdf+xml" -T "$file" "$l_graph_db_url/$db/data?graph=$graph"
   fi
   if [[ $? -ne 0 ]]; then
